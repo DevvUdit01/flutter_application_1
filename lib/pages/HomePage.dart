509704660d90055327483b03f98b67fb83a3f0dc
DevvUdit01 <devv.udit@gmail.com>
2023-10-08 21:20:30 +0530

    day 11 completed

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -2,10 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/widgets/drawer.dart';
 import 'package:cupertino_icons/cupertino_icons.dart';
 
+// day 11 we learned about context and constainsts
+
 class HomePage extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
+    String name="udit";
     return Scaffold(
       appBar: AppBar(
         title: Text("FlopKart"),
@@ -14,7 +17,7 @@ class HomePage extends StatelessWidget {
       body:Center(
         child: Container(
           child: 
-          Text("Welcome to FlopKart by udit",
+          Text("Welcome to FlopKart by $name",
           style: TextStyle(
             fontSize: 25,
             fontWeight: FontWeight.bold,
