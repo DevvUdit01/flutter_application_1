e8a276cec4005933e3d98e2baa7859be90b6fa52
DevvUdit01 <devv.udit@gmail.com>
2023-10-08 19:58:28 +0530

    day 10 completed

---
+++ lib/models/catalog.dart
@@ -0,0 +1,168 @@
+class item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  item({ required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
+  
+}
+
+final products= [
+
+   item(
+   id: "phone001",
+   name: "IPhone 15",
+   desc: "apple iphone 15th genration",
+   price: 89900,
+   color: "833505a",
+   image: "https://i.gadgets360cdn.com/products/small/iphone-15-plus-240x180-1694549219.jpg?downsize=*:90",
+),
+
+  item(
+   id: "phone002",
+   name: "IPhone 14",
+   desc: "apple iphone 14th genration",
+   price: 69900,
+   color: "833505a",
+   image: "https://i.gadgets360cdn.com/products/small/iphone-14-db-240x180-1662580917.jpg?downsize=*:90",
+),
+
+ item(
+   id: "phone003",
+   name: "IPhone 13",
+   desc: "apple iphone 13th genration",
+   price: 49900,
+   color: "833505a",
+   image: "https://i.gadgets360cdn.com/products/small/iphone-13-1-240x180-1631648722.jpg?downsize=*:90",
+),
+
+item(
+   id: "phone004",
+   name: "IPhone 12",
+   desc: "apple iphone 12th genration",
+   price: 48900,
+   color: "833505a",
+   image: "https://i.gadgets360cdn.com/products/small/iphone-12-all-240x180-1602616603.jpg?downsize=*:90",
+),
+
+item(
+   id: "phone005",
+   name: "IPhone 11",
+   desc: "apple iphone 11th genration",
+   price: 39900,
+   color: "833505a",
+   image: "https://i.gadgets360cdn.com/products/small/apple-iphone-11-240x180-1568197446.jpg?downsize=*:90",
+),
+
+item(
+   id: "phone006",
+   name: "IPhone XR",
+   desc: "apple iphone 10th genration",
+   price: 32900,
+   color: "833505a",
+   image: "https://i.gadgets360cdn.com/products/small/1536782796_120_iphone_xr.jpg?downsize=*:90",
+),
+
+item(
+   id: "phone007",
+   name: "IPhone X",
+   desc: "apple iphone 9th genration",
+   price: 64900,
+   color: "833505a",
+   image: "https://drop.ndtv.com/TECH/product_database/images/913201720154AM_120_iphone_x.jpeg?downsize=*:90",
+),
+
+item(
+   id: "phone008",
+   name: "IPhone 8",
+   desc: "apple iphone 8th genration",
+   price: 37999,
+   color: "833505a",
+   image: "https://drop.ndtv.com/TECH/product_database/images/913201715735AM_120_iphone_8.jpeg?downsize=*:90",
+),
+
+item(
+   id: "phone009",
+   name: "IPhone 7",
+   desc: "apple iphone 7th genration",
+   price: 24999,
+   color: "833505a",
+   image: "https://drop.ndtv.com/TECH/product_database/images/982016124827AM_120_iphone_7.jpeg?downsize=*:90",
+),
+
+item(
+   id: "phone010",
+   name: "IPhone SE",
+   desc: "apple iphone 6.5th genration",
+   price: 16999,
+   color: "833505a",
+   image: "https://drop.ndtv.com/TECH/product_database/images/3222016120932AM_120_apple_iphone_se.jpeg?downsize=*:90",
+),
+
+item(
+   id: "phone011",
+   name: "IPhone 6",
+   desc: "apple iphone 6th genration",
+   price: 15999,
+   color: "833505a",
+   image: "https://drop.ndtv.com/TECH/product_database/images/910201410302AM_120_apple_iphone_6.jpeg?downsize=*:90",
+),
+
+item(
+   id: "phone012",
+   name: "IPhone 5",
+   desc: "apple iphone 5th genration",
+   price: 4399,
+   color: "833505a",
+   image: "https://drop.ndtv.com/TECH/product_database/images/530201393615PM_120_apple_iphone_5.png?downsize=*:90",
+),
+
+item(
+   id: "phone013",
+   name: "IPhone 4",
+   desc: "apple iphone 4th genration",
+   price: 13999,
+   color: "833505a",
+   image: "https://drop.ndtv.com/TECH/product_database/images/530201374039PM_120_iPhone_4.png?downsize=*:90",
+),
+
+item(
+   id: "phone014",
+   name: "IPhone 3",
+   desc: "apple iphone 3th genration",
+   price: 12950,
+   color: "833505a",
+   image: "https://drop.ndtv.com/TECH/product_database/images/530201372119PM_120_iphone_3gs.png?downsize=*:90",
+),
+
+item(
+   id: "phone015",
+   name: "Apple IPhone",
+   desc: "apple iphone 1st genration",
+   price: 10850,
+   color: "833505a",
+   image: "https://drop.ndtv.com/TECH/product_database/images/530201361438PM_120_Apple_iPhone_first_gen.png?downsize=*:90",
+),
+
+// item(
+//    id: "phone001",
+//    name: "IPhone 14",
+//    desc: "apple iphone 14th genration",
+//    price: 69900,
+//    color: "833505a",
+//    image: "",
+// ),
+
+// item(
+//    id: "phone001",
+//    name: "IPhone 14",
+//    desc: "apple iphone 14th genration",
+//    price: 69900,
+//    color: "833505a",
+//    image: "",
+// ),
+
+];
